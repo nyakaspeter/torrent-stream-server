6d98d188940ae0a3daa49e98336013d7b93cf288
Péter Nyakas <thenyakaspeter@gmail.com>
2024-01-12 14:36:29 +0100

    add nCore search

--- src/search.ts
+++ src/search.ts
@@ -1,9 +1,10 @@
 import { searchJackett } from "./sources/jackett.js";
 import { JackettCategory } from "ts-jackett-api/lib/types/JackettCategory.js";
+import { NcoreCategory, searchNcore } from "./sources/ncore.js";
 
 export type TorrentCategory = "movie" | "show";
 
-export type TorrentSource = "jackett";
+export type TorrentSource = "jackett" | "ncore";
 
 export interface TorrentSearchOptions {
   categories?: TorrentCategory[];
@@ -11,6 +12,10 @@ export interface TorrentSearchOptions {
   jackett?: {
     url?: string;
     apiKey?: string;
+  };
+  ncore?: {
+    user?: string;
+    password?: string;
   };
 }
 
@@ -35,19 +40,49 @@ export const searchTorrents = async (
   const promises = [];
 
   if (options?.sources?.includes("jackett") || searchAllSources) {
-    const categories = [];
+    const categories = new Set<JackettCategory>();
 
-    if (options?.categories?.includes("movie") || searchAllCategories)
-      categories.push(JackettCategory.Movies);
-    if (options?.categories?.includes("show") || searchAllCategories)
-      categories.push(JackettCategory.TV);
+    if (options?.categories?.includes("movie") || searchAllCategories) {
+      categories.add(JackettCategory.Movies);
+    }
+
+    if (options?.categories?.includes("show") || searchAllCategories) {
+      categories.add(JackettCategory.TV);
+    }
 
     promises.push(
       searchJackett(
         query,
-        categories,
+        Array.from(categories),
         options?.jackett?.url,
         options?.jackett?.apiKey
+      )
+    );
+  }
+
+  if (options?.sources?.includes("ncore") || searchAllSources) {
+    const categories = new Set<NcoreCategory>();
+
+    if (options?.categories?.includes("movie") || searchAllCategories) {
+      categories.add(NcoreCategory.Film_HD_HU);
+      categories.add(NcoreCategory.Film_HD_EN);
+      categories.add(NcoreCategory.Film_SD_HU);
+      categories.add(NcoreCategory.Film_SD_EN);
+    }
+
+    if (options?.categories?.includes("show") || searchAllCategories) {
+      categories.add(NcoreCategory.Sorozat_HD_HU);
+      categories.add(NcoreCategory.Sorozat_HD_EN);
+      categories.add(NcoreCategory.Sorozat_SD_HU);
+      categories.add(NcoreCategory.Sorozat_SD_EN);
+    }
+
+    promises.push(
+      searchNcore(
+        query,
+        Array.from(categories),
+        options?.ncore?.user,
+        options?.ncore?.password
       )
     );
   }
